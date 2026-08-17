ffbdf0770d1dfc9f7104e9d774a41b7ee48a1d94
aatqabb <aatqagillani@gmail.com>
2026-08-17 23:13:11 +0500

    Install editor-wide control tooltips

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { installControlTooltips } from './controlTooltips.js'
+
+const removeTooltips = installControlTooltips()
+window.addEventListener('beforeunload', removeTooltips, { once: true })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
