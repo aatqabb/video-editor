4f1b9aa242d2888e8bd716143b7439ed6f82c9ae
aatqabb <aatqagillani@gmail.com>
2026-08-18 22:28:13 +0500

    Enable timeline refactor transform during dev and build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { timelineRefactorPlugin } from './scripts/vite-timeline-refactor-plugin.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [timelineRefactorPlugin(), react()],
 })
