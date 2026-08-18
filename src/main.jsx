84f25f90bb0c7a66e312f4d4b89df56fa5e5c8f5
aatqabb <aatqagillani@gmail.com>
2026-08-18 20:38:04 +0500

    Improve editor shortcuts and timeline UX
    * Make shortcut popup and editing tools more interactive
    
    * Polish playhead markers and shortcut interactions
    
    * Wire editor UX interaction enhancer

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './timelineEnhancements.css'
+import './editorUxEnhancer.css'
 import App from './App.jsx'
 import { installControlTooltips } from './controlTooltips.js'
 import { installTransitionPreviews } from './transitionPreviewEnhancer.js'
 import { installTimelineSelectionEnhancer } from './timelineSelectionEnhancer.js'
+import { installEditorUxEnhancer } from './editorUxEnhancer.js'
 
 const removeTooltips = installControlTooltips()
 const removeTransitionPreviews = installTransitionPreviews()
 const removeTimelineSelection = installTimelineSelectionEnhancer()
+const removeEditorUx = installEditorUxEnhancer()
 window.addEventListener('beforeunload', () => {
   removeTooltips()
   removeTransitionPreviews()
   removeTimelineSelection()
+  removeEditorUx()
 }, { once: true })
 
 createRoot(document.getElementById('root')).render(
