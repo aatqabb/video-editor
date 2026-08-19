e6474b6e1a7c1780f6c3b5375127830c51bd2287
aatqabb <aatqagillani@gmail.com>
2026-08-19 09:32:36 +0500

    Premiere-style playhead grab handle (#24)
    * Add Premiere-style dedicated playhead drag handle
    
    * Wire Premiere-style playhead plugin
    
    * Enlarge and smooth Premiere-style playhead handle
    
    * Make playhead handle bubble to captured parent drag

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { normalizeLineEndingsPlugin } from './scripts/normalize-line-endings-plugin.js'
 import { timelineRefactorPlugin } from './scripts/vite-timeline-refactor-plugin.js'
+import { premierePlayheadPlugin } from './scripts/vite-premiere-playhead-plugin.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [normalizeLineEndingsPlugin(), timelineRefactorPlugin(), react()],
+  plugins: [normalizeLineEndingsPlugin(), timelineRefactorPlugin(), premierePlayheadPlugin(), react()],
 })
