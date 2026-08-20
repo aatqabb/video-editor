dcbf341258f3d696b23fe43a7e463c498361b66d
aatqabb <aatqagillani@gmail.com>
2026-08-20 17:46:07 +0500

    Install editable shortcut manager

--- src/main.jsx
+++ src/main.jsx
@@ -9,18 +9,21 @@ import { installTransitionPreviews } from './transitionPreviewEnhancer.js'
 import { installTimelineSelectionEnhancer } from './timelineSelectionEnhancer.js'
 import { installEditorUxEnhancer } from './editorUxEnhancer.js'
 import { installTimelineScrubLock } from './timelineScrubLock.js'
+import { installShortcutManager } from './shortcutManager.js'
 
 const removeTooltips = installControlTooltips()
 const removeTransitionPreviews = installTransitionPreviews()
 const removeTimelineSelection = installTimelineSelectionEnhancer()
 const removeEditorUx = installEditorUxEnhancer()
 const removeTimelineScrubLock = installTimelineScrubLock()
+const removeShortcutManager = installShortcutManager()
 window.addEventListener('beforeunload', () => {
   removeTooltips()
   removeTransitionPreviews()
   removeTimelineSelection()
   removeEditorUx()
   removeTimelineScrubLock()
+  removeShortcutManager()
 }, { once: true })
 
 createRoot(document.getElementById('root')).render(
