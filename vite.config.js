7dc1729531d91bd107afa23eded754412cdc07ce
aatqabb <aatqagillani@gmail.com>
2026-08-20 17:08:45 +0500

    Wire smooth pointer timeline drag plugin

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { normalizeLineEndingsPlugin } from './scripts/normalize-line-endings-plugin.js'
 import { timelineRefactorPlugin } from './scripts/vite-timeline-refactor-plugin.js'
+import { smoothTimelineDragPlugin } from './scripts/vite-smooth-timeline-drag-plugin.js'
 import { premierePlayheadPlugin } from './scripts/vite-premiere-playhead-plugin.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [normalizeLineEndingsPlugin(), timelineRefactorPlugin(), premierePlayheadPlugin(), react()],
+  plugins: [normalizeLineEndingsPlugin(), timelineRefactorPlugin(), smoothTimelineDragPlugin(), premierePlayheadPlugin(), react()],
 })
